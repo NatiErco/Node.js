702f7caf4d8eaa65ac750271f10e4d910f160719
Natalia Ercolano <165024507+NatiErco@users.noreply.github.com>
2024-07-30 10:19:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Crea un script que genere el total de 5 + 6 cuando lo ejecutes con Node.js.
+
+function sum(a, b) {
+  return a + b;
+}
+
+const result = sum(5, 6);
+
+console.log("El resultado de 5 + 6 es", result);
